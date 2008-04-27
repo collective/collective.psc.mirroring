36189436ae75ee1bc7a00b04ae795e78fb8fe11f
tarek <tarek@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-04-27 22:34:45 +0000

    API beautification

--- collective/psc/mirroring/locker.py
+++ collective/psc/mirroring/locker.py
@@ -5,29 +5,40 @@ from os.path import join, dirname, basename, exists
 class AlreadyLocked(Exception):
     pass
 
-def _get_lock_name(path):
-    name = basename(path)
-    return join(dirname(path), '%s_lck' % name)
+def _get_lock_name(filename):
+    name = basename(filename)
+    return join(dirname(filename), '%s_lck' % name)
 
-def with_lock(path, mode, callable):
-    file = open(path, mode)
-    file_lock = _get_lock_name(path)
+def with_lock(filename, mode, callable_):
+    """Uses fcntl to lock a file, open it, then
+    call the callable_ with the file object.
+    
+    filename is the file to lock, mode is the open mode.
+
+    The file is closed when the callable returns.
+    """
+    file_ = open(filename, mode)
+    file_lock = _get_lock_name(filename)
     if exists(file_lock):
-        raise AlreadyLocked('%s is already locked.' % path)
+        raise AlreadyLocked('%s is already locked.' % filename)
     open(file_lock, 'w').write('locked')
-    fcntl.flock(file.fileno(), fcntl.LOCK_EX)
+    fcntl.flock(file_.fileno(), fcntl.LOCK_EX)
     try:
-        callable(file) 
+        callable_(file_) 
     finally:
-        file.close()
+        file_.close()
         os.remove(file_lock)
 
-def is_locked(path):
-    return exists(_get_lock_name(path)) 
+def is_locked(filename):
+    """Returns True if the filename is locked.""" 
+    return exists(_get_lock_name(filename)) 
 
-def write_content(path, stream):
+def write_content(filename, stream):
+    """Writes stream content into filename.
+    
+    stream can be an iterator or an open file object."""
     def _write(f):
         for line in stream:
             f.write(line)
-    with_lock(path, 'wb', _write)
+    with_lock(filename, 'wb', _write)
 
